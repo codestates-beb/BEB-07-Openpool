502c1ed206657e99a5788a45ac645639c1f5cb01
Duhoon <eclip6@naver.com>
2022-12-23 20:59:41 +0900

    feat: log add for dev

--- openpool-server/src/app.ts
+++ openpool-server/src/app.ts
@@ -1,9 +1,13 @@
-import express  from "express";
+import express from "express";
 import {Request, Response} from "express";
+import morgan from 'morgan';
+
 const app = express();
 require("dotenv").config();
 
 const PORT = process.env.PORT || 4000;
+
+app.use(morgan("dev"));
 
 app.get("", (req: Request, res:Response)=>{
     console.log("Hello TypeScripts");
