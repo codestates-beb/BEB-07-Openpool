04c58b1fb62061828b1ddbd2a76877350b16279a
Duhoon <eclip6@naver.com>
2022-12-23 18:40:31 +0900

    init: client init

---
+++ openpool-client/src/App.js
@@ -0,0 +1,32 @@
+// Library
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import './App.css';
+
+// Components
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+// Pages
+import MainPage from "./pages/MainPage";
+import MyPage from "./pages/MyPage";
+import DetailPage from "./pages/DetailPage";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+        <Routes>
+          <Route path="/" element={<MainPage/>}/>
+          <Route path="/mypage" element={<MyPage/>}/>
+          <Route path="/detail" element={<DetailPage/>}>
+              <Route path=":id"/>
+          </Route>
+          <Route path="/" element={<MainPage/>}/>
+        </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ openpool-client/src/pages/MainPage.js
@@ -0,0 +1,9 @@
+const MainPage = ()=>{
+    return (
+        <div className="flex justify-center">
+            <p className="text-2xl font-bold"> Project Start </p>
+        </div>
+    )
+}
+
+export default MainPage;
