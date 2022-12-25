3a20ef5ff5655eb994d8b4c6b28fa866bde2e3e9
Duhoon <eclip6@naver.com>
2022-12-25 16:14:58 +0900

    feat: brand logo 이미지 추가 및 헤더 적용 openpool brandlogo 이미지를 프로젝트 내에 추가하고 헤더에 적용하여 view에 반영했습니다.

--- openpool-client/src/components/Header.js
+++ openpool-client/src/components/Header.js
@@ -1,9 +1,11 @@
 // modules
 import {useState} from "react";
-
 import {Link} from "react-router-dom";
-import WalletIcon from "@heroicons/react/24/solid/WalletIcon";
-import ShopingCartIcon from "@heroicons/react/24/solid/ShoppingCartIcon"
+
+// Heroicons
+import WalletIcon from "@heroicons/react/24/outline/WalletIcon";
+import ShopingCartIcon from "@heroicons/react/24/outline/ShoppingCartIcon"
+import UserCircleIcon from "@heroicons/react/24/outline/UserCircleIcon";
 
 // css
 import "../assets/css/header.css";
@@ -11,9 +13,9 @@ import "../assets/css/header.css";
 const Header = ()=>{
     return (
         <header className="py-4 px-16 w-full flex justify-between items-center fixed top-0">
-            <div className="logo-group">
-                <div className="logo-wrapper">
-                    <img src=""/>
+            <div className="logo-group flex items-center">
+                <div className="logo-wrapper h-10 w-10 mr-2">
+                    <img className="w-full " src={process.env.PUBLIC_URL + "/images/logo.png"}/>
                 </div>
                 <div className="logo-brand">
                     <Link className="text-2xl font-bold">OpenPool</Link>
@@ -24,7 +26,12 @@ const Header = ()=>{
             </div>
             <navbar>
                 <ul className="link-group flex items-center">
-                    <li className="link-item text-xl font-bold"><Link>Explore</Link></li>
+                    <li className="link-item"><Link>Explore</Link></li>
+                    <li className="link-item">
+                        <Link>
+                            <UserCircleIcon className="h-10 w-10"/>
+                        </Link>
+                    </li>
                     <li className="link-item">
                         <Link>
                             <WalletIcon className="h-10 w-10"/>
@@ -32,7 +39,7 @@ const Header = ()=>{
                     </li>
                     <li className="link-item">
                         <Link>
-                            <ShopingCartIcon className="h-10 w-10"></ShopingCartIcon>
+                            <ShopingCartIcon className="h-10 w-10"/>
                         </Link>
                     </li>
                 </ul>
