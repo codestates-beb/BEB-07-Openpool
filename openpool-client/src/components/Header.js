b3220d04704d927478e07936d91bdfb59286999a
Duhoon <eclip6@naver.com>
2022-12-25 15:42:48 +0900

    feat: 헤더 요소 별 간격 추가 헤더 요소 간 간격 조정으로 UI 개선

--- openpool-client/src/components/Header.js
+++ openpool-client/src/components/Header.js
@@ -1,7 +1,9 @@
-// module
+// modules
+import {useState} from "react";
+
+import {Link} from "react-router-dom";
 import WalletIcon from "@heroicons/react/24/solid/WalletIcon";
 import ShopingCartIcon from "@heroicons/react/24/solid/ShoppingCartIcon"
-import {Link} from "react-router-dom";
 
 // css
 import "../assets/css/header.css";
@@ -17,18 +19,18 @@ const Header = ()=>{
                     <Link className="text-2xl font-bold">OpenPool</Link>
                 </div>
             </div>
-            <div className="searchbar-wrapper w-full">
+            <div className="searchbar-wrapper w-full px-16">
                 <input className="searchbar" placeholder="Search items, collections, and accounts"></input>
             </div>
             <navbar>
-                <ul className="flex items-center">
-                    <li><Link>Explore</Link></li>
-                    <li>
+                <ul className="link-group flex items-center">
+                    <li className="link-item text-xl font-bold"><Link>Explore</Link></li>
+                    <li className="link-item">
                         <Link>
                             <WalletIcon className="h-10 w-10"/>
                         </Link>
                     </li>
-                    <li>
+                    <li className="link-item">
                         <Link>
                             <ShopingCartIcon className="h-10 w-10"></ShopingCartIcon>
                         </Link>
