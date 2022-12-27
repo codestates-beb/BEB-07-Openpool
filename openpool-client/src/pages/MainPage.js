2ea42e2e87780d390d7e39aab617a7d682b7f5dc
jchoo0x <jchoo0x@icloud.com>
2022-12-27 17:32:36 +0900

    메인페이지 디자인

--- openpool-client/src/pages/MainPage.js
+++ openpool-client/src/pages/MainPage.js
@@ -4,6 +4,25 @@ import { Keyboard, FreeMode, Navigation } from "swiper";
 import "swiper/css";
 import "swiper/css/navigation";
 
+// Images
+import whatIsNft from "../assets/images/what-is-nft.png";
+import whatIsCryptoWallet from "../assets/images/what-is-crypto-wallet.png";
+import whatAreGasFees from "../assets/images/what-are-gas-fees.png";
+import whatIsMinting from "../assets/images/what-is-minting.png";
+import whoIsOpenSea from "../assets/images/who-is-opensea.png";
+import howToBuyNft from "../assets/images/how-to-buy-nft.png";
+import howToCreateNft from "../assets/images/how-to-create-nft.png";
+import howToSellNft from "../assets/images/how-to-sell-nft.png";
+import art from "../assets/images/art.png";
+import collectibles from "../assets/images/collectibles.png";
+import music from "../assets/images/music.png";
+import domainNames from "../assets/images/domain-names.png";
+import photography from "../assets/images/photography-category.png";
+import sports from "../assets/images/sports.png";
+import tradingCards from "../assets/images/trading-cards.png";
+import utility from "../assets/images/utility.png";
+import virtualWorlds from "../assets/images/virtual-worlds.png";
+
 // 위의 swiper를 이용해서 슬라이드를 구현하세요!
 // swiper를 이용하면 손쉽게 슬라이드를 구현할 수 있습니다.
 // 참고로 OpenSea도 swiper를 이용하고 있어서 여러 프로젝트에 사용되고 있다는 것을 알 수 있습니다.
@@ -12,18 +31,18 @@ import "swiper/css/navigation";
 const MainPage = () => {
   return (
     <>
-      <div className="flex justify-center">
+      <div className="pt-10 flex justify-center">
         <p className="text-5xl font-extrabold">
           Explore, collect, and sell NFTs
         </p>
       </div>
       <div className="main"></div>
       <div>
-        <h1 className="text-xl font-bold">NFT 101</h1>
-        <p>Get comfortable with the basics.</p>
+        <h1 className="pt-10 pl-4 pb-2 text-xl font-bold">NFT 101</h1>
+        <p className="pl-4 pb-4">Get comfortable with the basics.</p>
         <Swiper
           slidesPerView={5}
-          spaceBetween={30}
+          spaceBetween={20}
           slidesPerGroup={5}
           loop={true}
           keyboard={{
@@ -35,58 +54,248 @@ const MainPage = () => {
           className="mySwiper"
         >
           <SwiperSlide>
-            <a href="https://opensea.io/learn/what-are-nfts" rel="noreferrer">
-              What is an NFT?
-            </a>
+            <div className="max-w-sm rounded-xl overflow-hidden shadow-lg">
+              <a href="https://opensea.io/learn/what-are-nfts" rel="noreferrer">
+                <img className="w-full" src={whatIsNft} alt="" />
+                <div className="px-6 py-4">
+                  <div className="font-bold text-l mb-4">What is an NFT?</div>
+                </div>
+              </a>
+            </div>
           </SwiperSlide>
           <SwiperSlide>
-            <a
-              href="https://opensea.io/learn/what-is-crypto-wallet"
-              rel="norefferer"
-            >
-              What is a crypto wallet?
-            </a>
+            <div className="max-w-sm rounded-xl overflow-hidden shadow-lg">
+              <a
+                href="https://opensea.io/learn/what-is-crypto-wallet"
+                rel="noreferrer"
+              >
+                <img className="w-full" src={whatIsCryptoWallet} alt="" />
+                <div className="px-6 py-4">
+                  <div className="font-bold text-l mb-4">
+                    What is a crypto wallet?
+                  </div>
+                </div>
+              </a>
+            </div>
           </SwiperSlide>
           <SwiperSlide>
-            <a href="https://opensea.io/learn/nft-gas-fees" rel="norefferer">
-              What are blockchain gas fees?
-            </a>
+            <div className="max-w-sm rounded-xl overflow-hidden shadow-lg">
+              <a href="https://opensea.io/learn/nft-gas-fees" rel="noreferrer">
+                <img className="w-full" src={whatAreGasFees} alt="" />
+                <div className="px-6 py-4">
+                  <div className="font-bold text-l mb-4">
+                    What are blockchain gas fees?
+                  </div>
+                </div>
+              </a>
+            </div>
           </SwiperSlide>
           <SwiperSlide>
-            <a href="https://opensea.io/learn/how-to-buy-nft" rel="norefferer">
-              How to buy an NFT
-            </a>
+            <div className="max-w-sm rounded-xl overflow-hidden shadow-lg">
+              <a
+                href="https://opensea.io/learn/how-to-buy-nft"
+                rel="noreferrer"
+              >
+                <img className="w-full" src={howToBuyNft} alt="" />
+                <div className="px-6 py-4">
+                  <div className="font-bold text-l mb-4">How to buy an NFT</div>
+                </div>
+              </a>
+            </div>
           </SwiperSlide>
           <SwiperSlide>
-            <a
-              href="https://opensea.io/learn/how-to-create-an-nft"
-              rel="norefferer"
-            >
-              How to create an NFT on OpenSea
-            </a>
+            <div className="max-w-sm rounded-xl overflow-hidden shadow-lg">
+              <a
+                href="https://opensea.io/learn/how-to-create-an-nft"
+                rel="noreferrer"
+              >
+                <img className="w-full" src={howToCreateNft} alt="" />
+                <div className="px-6 py-4">
+                  <div className="font-bold text-l mb-2">
+                    How to create an NFT on OpenSea
+                  </div>
+                </div>
+              </a>
+            </div>
           </SwiperSlide>
           <SwiperSlide>
-            <a
-              href="https://opensea.io/learn/how-to-sell-nfts"
-              rel="norefferer"
-            >
-              How to sell an NFT using OpenSea
-            </a>
+            <div className="max-w-sm rounded-xl overflow-hidden shadow-lg">
+              <a
+                href="https://opensea.io/learn/how-to-sell-nfts"
+                rel="noreferrer"
+              >
+                <img className="w-full" src={howToSellNft} alt="" />
+                <div className="px-6 py-4">
+                  <div className="font-bold text-l mb-2">
+                    How to sell an NFT using OpenSea
+                  </div>
+                </div>
+              </a>
+            </div>
           </SwiperSlide>
           <SwiperSlide>
-            <a
-              href="https://opensea.io/learn/what-is-minting-nft"
-              rel="norefferer"
-            >
-              What is minting?
-            </a>
+            <div className="max-w-sm rounded-xl overflow-hidden shadow-lg">
+              <a
+                href="https://opensea.io/learn/what-is-minting-nft"
+                rel="noreferrer"
+              >
+                <img className="w-full" src={whatIsMinting} alt="" />
+                <div className="px-6 py-4">
+                  <div className="font-bold text-l mb-2">What is minting?</div>
+                </div>
+              </a>
+            </div>
           </SwiperSlide>
           <SwiperSlide>
-            <a href="https://opensea.io/learn/who-is-opensea" rel="norefferer">
-              Who is OpenSea?
-            </a>
+            <div className="max-w-sm rounded-xl overflow-hidden shadow-lg">
+              <a
+                href="https://opensea.io/learn/who-is-opensea"
+                rel="noreferrer"
+              >
+                <img className="w-full" src={whoIsOpenSea} alt="" />
+                <div className="px-6 py-6">
+                  <div className="font-bold text-l mb-4">Who is OpenSea?</div>
+                </div>
+              </a>
+            </div>
           </SwiperSlide>
         </Swiper>
+        <div>
+          <h1 className="pt-10 pl-4 pb-4 text-xl font-bold">
+            Browse by category
+          </h1>
+          <Swiper
+            slidesPerView={5}
+            spaceBetween={20}
+            slidesPerGroup={5}
+            loop={true}
+            keyboard={{
+              enabled: true,
+            }}
+            freeMode={true}
+            navigation={true}
+            modules={[Keyboard, FreeMode, Navigation]}
+            className="mySwiper"
+          >
+            <SwiperSlide>
+              <div className="max-w-sm rounded-xl overflow-hidden shadow-lg">
+                <a
+                  href="https://testnets.opensea.io/category/art"
+                  rel="noreferrer"
+                >
+                  <img className="w-full" src={art} alt="" />
+                  <div className="px-6 py-4">
+                    <div className="font-bold text-l mb-4">Art</div>
+                  </div>
+                </a>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="max-w-sm rounded-xl overflow-hidden shadow-lg">
+                <a
+                  href="https://testnets.opensea.io/category/collectibles"
+                  rel="noreferrer"
+                >
+                  <img className="w-full" src={collectibles} alt="" />
+                  <div className="px-6 py-4">
+                    <div className="font-bold text-l mb-4">Collectibles</div>
+                  </div>
+                </a>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="max-w-sm rounded-xl overflow-hidden shadow-lg">
+                <a
+                  href="https://testnets.opensea.io/category/domain-names"
+                  rel="noreferrer"
+                >
+                  <img className="w-full" src={domainNames} alt="" />
+                  <div className="px-6 py-4">
+                    <div className="font-bold text-l mb-4">Domain Names</div>
+                  </div>
+                </a>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="max-w-sm rounded-xl overflow-hidden shadow-lg">
+                <a
+                  href="https://testnets.opensea.io/category/music"
+                  rel="noreferrer"
+                >
+                  <img className="w-full" src={music} alt="" />
+                  <div className="px-6 py-4">
+                    <div className="font-bold text-l mb-4">Music</div>
+                  </div>
+                </a>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="max-w-sm rounded-xl overflow-hidden shadow-lg">
+                <a
+                  href="https://testnets.opensea.io/category/photography-category"
+                  rel="noreferrer"
+                >
+                  <img className="w-full" src={photography} alt="" />
+                  <div className="px-6 py-4">
+                    <div className="font-bold text-l mb-4">Photography</div>
+                  </div>
+                </a>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="max-w-sm rounded-xl overflow-hidden shadow-lg">
+                <a
+                  href="https://testnets.opensea.io/category/sports"
+                  rel="noreferrer"
+                >
+                  <img className="w-full" src={sports} alt="" />
+                  <div className="px-6 py-4">
+                    <div className="font-bold text-l mb-4">Sports</div>
+                  </div>
+                </a>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="max-w-sm rounded-xl overflow-hidden shadow-lg">
+                <a
+                  href="https://testnets.opensea.io/category/trading-cards"
+                  rel="noreferrer"
+                >
+                  <img className="w-full" src={tradingCards} alt="" />
+                  <div className="px-6 py-4">
+                    <div className="font-bold text-l mb-4">Trading Cards</div>
+                  </div>
+                </a>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="max-w-sm rounded-xl overflow-hidden shadow-lg">
+                <a
+                  href="https://testnets.opensea.io/category/utility"
+                  rel="noreferrer"
+                >
+                  <img className="w-full" src={utility} alt="" />
+                  <div className="px-6 py-4">
+                    <div className="font-bold text-l mb-4">Utility</div>
+                  </div>
+                </a>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="max-w-sm rounded-xl overflow-hidden shadow-lg">
+                <a
+                  href="https://testnets.opensea.io/category/virtual-worlds"
+                  rel="noreferrer"
+                >
+                  <img className="w-full" src={virtualWorlds} alt="" />
+                  <div className="px-6 py-4">
+                    <div className="font-bold text-l mb-4">Virtual Worlds</div>
+                  </div>
+                </a>
+              </div>
+            </SwiperSlide>
+          </Swiper>
+        </div>
       </div>
     </>
   );
--- openpool-client/src/pages/MintingPage.js
+++ openpool-client/src/pages/MintingPage.js
@@ -1,52 +1,46 @@
-import React, {useEffect, useState, useRef} from 'react';
-import axios from 'axios';
-import web3 from 'web3';
+import React, { useEffect, useState, useRef } from "react";
+import axios from "axios";
 import "../assets/css/minting.css";
 
-
-
-
-// 이름 링크 설명 블록체인 
+// 이름 링크 설명 블록체인
 function Minting() {
+  const [file, setFile] = useState(null); // 초기 사진 빈 값
+  const [descrition, setDescription] = useState(""); // 설명 초기값
+  const [name, setName] = useState(""); // 이름 초기값
+  const [link, setLink] = useState(""); // 링크 초기값
+  const [chain, setChain] = useState(""); // 체인 초기값
+  const blockChainList = ["Ethereum", "Klaytn", "Solana"];
 
-    const [file, setFile] = useState(null); // 초기 사진 빈 값
-    const [descrition, setDescription] = useState(""); // 설명 초기값
-    const [name, setName] = useState("") // 이름 초기값
-    const [link, setLink] = useState("") // 링크 초기값
-    const [chain, setChain] = useState("") // 체인 초기값 
-    const blockChainList = ["Ethereum", "Klaytn", "Solana"];
+  const fileUploader = useRef(null); // 사진 업로드
 
-    const fileUploader = useRef(null); // 사진 업로드
+  const handleClick = (e) => {
+    fileUploader.current.click();
+  };
 
-    const handleClick = (e) => {
-        fileUploader.current.click();
+  const handleChange = (e) => {
+    const maxSize = 100 * 1024 * 1024; // 최대 크기 지정
+
+    if (e.target.files[0].size > maxSize) {
+      alert("첨부파일 사이즈는 100MB 이내로 등록 가능합니다.");
+    } else {
+      //add file handler
+      //console.log(e.target.files[0]);
+      setFile(e.target.files[0]);
     }
-    
-    const handleChange = (e) => {
-        const maxSize = 100 * 1024 * 1024; // 최대 크기 지정
-    
-        if (e.target.files[0].size > maxSize) {
-          alert("첨부파일 사이즈는 100MB 이내로 등록 가능합니다.")
-        } else {
-          //add file handler
-          //console.log(e.target.files[0]);
-          setFile(e.target.files[0]);
-        }
+  };
+  const checkElement = () => {
+    if (name && file && chain) {
+      return true;
     }
-    const checkElement = () => {
-        if (name && file && chain) {
-            return true;
-        }
-        return false;
-    }
-    return(
-        <>
-        <div>
-            <h1>ㅎㅇㅎㅇ</h1>
-        </div>
-        </>
-    )
+    return false;
+  };
+  return (
+    <>
+      <div>
+        <h1>ㅎㅇㅎㅇ</h1>
+      </div>
+    </>
+  );
 }
 
-export default Minting
-
+export default Minting;
