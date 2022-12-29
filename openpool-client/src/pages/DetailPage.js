b6989750e878067fed5fc6eb5a1e45560434fba8
Duhoon <eclip6@naver.com>
2022-12-29 19:15:18 +0900

    feat: 디테일 빈 페이지 구현

--- openpool-client/src/pages/DetailPage.js
+++ openpool-client/src/pages/DetailPage.js
@@ -52,13 +52,13 @@ const DetailPage = () => {
     
 <section className="text-gray-600 body-font">
   
-  <div className="container px-5 py-24 mx-auto flex flex-col content-center p-2">
+  <div className="container border rounded-lg px-5 py-24 mx-auto flex flex-col content-center p-2">
     <h2 className="font-bold title-font mb-8 text-gray-900 text-2xl text-center">
-      {nftName}
+      {nftName ? nftName: "항목 없음"}
     </h2>
     <div className="lg:w-4/6 mx-auto">
-      <div className="rounded-lg h-64 overflow-hidden">
-        <img className="object-cover object-center h-full w-full" src={img} alt="" />
+      <div className="rounded-lg border h-64 overflow-hidden">
+        <img className="object-cover object-center h-full w-full" src={img ? img : process.env.PUBLIC_URL + "/images/none.png"} alt="" />
       </div>
       <div className="flex flex-col sm:flex-row mt-10">
         <div className="sm:w-1/3 text-center sm:pr-8 sm:py-8">
@@ -80,7 +80,7 @@ const DetailPage = () => {
         </div>
         <div className="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
           <p className="leading-relaxed text-lg mb-4"> 
-            {description}
+            {description ? description: "항목 없음"}
           </p>          
         </div>
         
